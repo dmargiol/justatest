c7a815bcc54f5a3e4dec91ba17170ac4da4146f6
dmargiol <dmargiol@student.tgm.ac.at>
2016-08-25 13:54:23 +0200

    1st commit

---
+++ T05/src/SchulKlasse.java
@@ -0,0 +1,12 @@
+
+public class SchulKlasse {
+	
+public int SchuelerAnzahl;
+
+public String KlassenName;
+
+	public SchulKlasse(String KlassenName, int SchuelerAnzahl){
+		this.KlassenName = KlassenName;
+		this.SchuelerAnzahl = SchuelerAnzahl;
+	}
+}
---
+++ T05/src/TGMHIT.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.LinkedList;
+
+public class TGMHIT {
+	
+	public TGMHIT(){
+		ArrayList<SchulKlasse> al = new ArrayList<SchulKlasse>();
+		SchulKlasse klasse1 = new SchulKlasse("1AHIT",23);
+		al.add(klasse1);
+	}
+	public void main(){
+		
+	}
+}
